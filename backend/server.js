4f68c0d935c7b44cadc0e7512bdea888758b333c
IT20127800 <pamithaa19@gmail.com>
2023-09-12 10:35:21 +0530

    backend add

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
+
+
+const app = express();
+
+//import route
+const GroupRoutes = require('./Routes/GroupRegister');
+
+
+
+//app middleware
+app.use(bodyParser.json());
+app.use(cors());
+//app.use(multer());
+
+
+app.use(GroupRoutes);
+
+
+
+
+const PORT = 8001;
+const DB_URL = 'mongodb+srv://pamitha:pamitha@database1.gqpga.mongodb.net/Ticket-Reservation-System?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL)
+.then(() =>{
+    console.log('DB connected');
+}).catch((err) => console.log('DB connection error', err));
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`App is running on ${PORT}`);
+});
+
